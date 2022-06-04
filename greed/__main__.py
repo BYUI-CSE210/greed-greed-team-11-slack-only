279869283596445a1dc99a882d3d276d4d63d0d1
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-04 17:49:56 -0500

    Update __main__.py

--- greed/__main__.py
+++ greed/__main__.py
@@ -2,8 +2,9 @@ import os
 import random
 
 from game.casting.actor import Actor
-from game.casting.artifact import Artifact
+from game.casting.stone import Stone
 from game.casting.cast import Cast
+from game.casting.score import Score
 
 from game.directing.director import Director
 
@@ -13,79 +14,43 @@ from game.services.video_service import VideoService
 from game.shared.color import Color
 from game.shared.point import Point
 
-
-FRAME_RATE = 12
+FRAME_RATE = 30
 MAX_X = 900
 MAX_Y = 600
 CELL_SIZE = 15
-FONT_SIZE = 45
+FONT_SIZE = 20
 COLS = 60
 ROWS = 40
-CAPTION = "GREED"
+CAPTION = "Greed"
 WHITE = Color(255, 255, 255)
-DEFAULT_ARTIFACTS = 100
-
+RED = Color(255, 0, 0)
 
 def main():
-
+    
     # create the cast
     cast = Cast()
-
-    # create the banner
-    banner = Actor()
-    banner.set_text("The Score")
-    banner.set_font_size(FONT_SIZE)
-    banner.set_color(WHITE)
-    banner.set_position(Point(CELL_SIZE, 0))
-    cast.add_actor("banners", banner)
-
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
-    position = Point(x, y)
-
-    robot = Actor()
-    robot.set_text("|")
-    robot.set_font_size(FONT_SIZE)
-    robot.set_color(WHITE)
-    robot.set_position(position)
-    cast.add_actor("robots", robot)
+    y = int(570)
+    position = Point(x, y) 
+    #create the banner
+    banner = Actor()
+    banner.set_text("@")
+    banner.set_font_size(FONT_SIZE)
+    banner.set_color(RED)
+    banner.set_position(position)
+    cast.add_actor("banner", banner)
+    # create intial score
+    score = Score()
+    score.set_position(Point(MAX_X // 2, 15))
+    score.set_color(RED)
+    cast.add_actor("score", score)
 
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
-    video_service = VideoService(
-        CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
     director = Director(keyboard_service, video_service)
     director.start_game(cast)
-
-    # create the artifacts
-    for n in range(DEFAULT_ARTIFACTS):
-        shape = random.choice(['*','o'])
-        message = ''
-        if shape == '*':
-            message = '1 point'
-        else:
-            message = '-1 point'
-
-
-        x = random.randint(1, COLS - 1)
-        y = random. randint(1, ROWS - 1)
-
-        position = Point(x, y)
-        position = position.scale(CELL_SIZE)
-
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
-
-        artifact = Artifact()
-        artifact.set_shape(shape)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
 
 if __name__ == "__main__":
     main()
