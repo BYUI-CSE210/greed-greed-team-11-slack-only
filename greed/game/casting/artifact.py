72d1637961f038c3c873a6459f749b8c994f1347
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-04 17:47:07 -0500

    score.py

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -1,31 +1,31 @@
 from game.casting.actor import Actor
 
 
-class Artifact(Actor):
+class Score(Actor):
     """
-    A falling object.
+    An item of cultural or historical interest. 
     
     The responsibility of an Artifact is to provide a message about itself.
-
     Attributes:
         _message (string): A short description about the artifact.
     """
     def __init__(self):
         super().__init__()
-        self._message = ""
-        
-    def get_message(self):
-        """Gets the artifact's message.
+        self._points = 0
+
+    def get_points(self):
+        """Returns the current score.
         
         Returns:
-            string: The message.
+            int: The current score.
         """
-        return self._message
-    
-    def set_message(self, message):
+        return self._points
+
+    def add_points(self, points):
         """Updates the message to the given one.
         
         Args:
             message (string): The given message.
         """
-        self._message = message
+        self._points += points
+        self.set_text(f"Score: {self._points}")
