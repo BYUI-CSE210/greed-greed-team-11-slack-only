7b513c2a7ab32685be0489f812836de75ed94c34
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-04 20:37:03 -0500

    Update director.py

--- greed/game/directing/director.py
+++ greed/game/directing/director.py
@@ -1,7 +1,8 @@
+import random as r
 from game.casting.stone import Stone
 from game.shared.point import Point
 from game.shared.color import Color
-import random as r
+
 # from game.casting.cast import Cast
 
 WHITE = Color(255, 255, 255)
@@ -44,11 +45,11 @@ class Director:
             cast (Cast): The cast of actors.
         """
         # get direction to move from keyboard service
-        banner = cast.get_first_actor("banner")
+        user = cast.get_first_actor("user")
         velocity = self._keyboard_service.get_direction()
-        banner.set_velocity(velocity)  
-        # banner.set_velocity(Point(-15, 0))      
-        # banner actor = direction horizontal only
+        user.set_velocity(velocity)  
+        # user.set_velocity(Point(-15, 0))      
+        # user actor = direction horizontal only
 
     def _do_updates(self, cast):
         """Updates the robot's position and resolves any collisions with artifacts.
@@ -76,37 +77,37 @@ class Director:
         cast.add_actor("stones", gem)
         cast.add_actor("stones", rock)
 
-        # move the three actors player or banner, rocks and gems
+        # move player, rocks, gems
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
-        # loop to move the actors
-        banner = cast.get_first_actor("banner")
-        banner_x = banner.get_position().get_x()
-        banner_y = banner.get_position().get_y()
+        # loop to move all actors
+        user = cast.get_first_actor("user")
+        user_x = user.get_position().get_x()
+        user_y = user.get_position().get_y()
         score = cast.get_first_actor("score")
         for actor in cast.get_actors("stones"):
             actor.move_next(max_x, max_y)
-            # check for updates
+            # check for collisions
             if actor.get_text() == "*":
                 actor_x = actor.get_position().get_x()
                 actor_y = actor.get_position().get_y()
-                if ((banner_x - 10 < actor_x < banner_x + 10) and (banner_y - 10 < actor_y < banner_y + 10)):
+                if ((user_x - 10 < actor_x < user_x + 10) and (user_y - 10 < actor_y < user_y + 10)):
                     score.add_points(1)
-                if actor_y > max_y - 30 or((banner_x - 10 < actor_x < banner_x + 10) and (banner_y - 10 < actor_y < banner_y + 10)):
+                if actor_y > max_y - 30 or((user_x - 10 < actor_x < user_x + 10) and (user_y - 10 < actor_y < user_y + 10)):
                     cast.remove_actor("stones", actor)
                     
             elif actor.get_text() == "o":
                 actor_x = actor.get_position().get_x()
                 actor_y = actor.get_position().get_y()
-                if ((banner_x - 10 < actor_x < banner_x + 10) and (banner_y - 10 < actor_y <banner_y + 10)):
+                if ((user_x - 10 < actor_x < user_x + 10) and (user_y - 10 < actor_y <user_y + 10)):
                     score.add_points(-1)
-                if actor_y > max_y - 30 or ((banner_x - 10 < actor_x < banner_x + 10) and (banner_y - 10 < actor_y < banner_y + 10)):
+                if actor_y > max_y - 30 or ((user_x - 10 < actor_x < user_x + 10) and (user_y - 10 < actor_y < user_y + 10)):
                     cast.remove_actor("stones", actor)
                     
                     
             
         # move player
-        banner.move_next(max_x, max_y)
+        user.move_next(max_x, max_y)
 
         
     def _do_outputs(self, cast):
@@ -116,11 +117,12 @@ class Director:
             cast (Cast): The cast of actors.
         """
         self._video_service.clear_buffer()
-        banner = cast.get_first_actor("banner")
+        # stones = cast.get_actors("stones")
+        user = cast.get_first_actor("user")
         score = cast.get_first_actor("score")
         score.set_text(f"SCORE: {score.get_points()}")
         for actor in cast.get_actors("stones"):
             self._video_service.draw_actor(actor)
-        self._video_service.draw_actor(banner)
+        self._video_service.draw_actor(user)
         self._video_service.draw_actor(score)
         self._video_service.flush_buffer()
